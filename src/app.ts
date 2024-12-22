f4e6b4c64ed098dc0c52fcd8eca0caedeae0d309
Shareef <shareef@kmzee.online>
2024-12-22 15:40:21 +0530

    init: not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { logger } from "hono/logger";
 import { Hono } from "hono";
+import { notFound } from "./middlewares/not-found.ts";
 
 const app = new Hono({ strict: false });
 
 app.use(logger());
+app.notFound(notFound);
 
 export default app;
